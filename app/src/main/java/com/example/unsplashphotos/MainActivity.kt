3500613bdee12e6256abe902556a7ee852416309
Jahid Hasan Naiem <jhnaiem96@gmail.com>
2021-12-02 13:24:59 +0600

    initial commit

---
+++ app/src/main/java/com/example/unsplashphotos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.unsplashphotos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
