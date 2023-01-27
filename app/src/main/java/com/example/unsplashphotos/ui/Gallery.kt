442b6772b6ad8095d30b2db6d8107ba5940e68c9
Jahid Hasan Naiem <jahid.hasan@monstar-lab.com>
2023-01-27 12:12:47 +0600

    feat(Gallery): Add load and error view

--- app/src/main/java/com/example/unsplashphotos/ui/Gallery.kt
+++ app/src/main/java/com/example/unsplashphotos/ui/Gallery.kt
@@ -10,10 +10,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -29,12 +31,12 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.paging.LoadState
 import androidx.paging.PagingData
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.AsyncImage
@@ -46,6 +48,9 @@ import com.example.unsplashphotos.R.string
 import com.example.unsplashphotos.domain.model.Photo
 import com.example.unsplashphotos.ui.theme.UnsplashTheme
 import com.example.unsplashphotos.ui.theme.UnsplashTypography
+import com.google.accompanist.placeholder.PlaceholderHighlight
+import com.google.accompanist.placeholder.material.placeholder
+import com.google.accompanist.placeholder.material.shimmer
 import kotlinx.coroutines.flow.Flow
 
 @Composable
@@ -62,11 +67,56 @@ private fun PhotoGriding(
         contentPadding = innerPadding,
         state = rememberLazyGridState()
     ) {
-        items(photos.itemCount) { index ->
+        when (val state = photos.loadState.prepend) {
+            is LoadState.NotLoading -> Unit
+            is LoadState.Loading -> {
+                Loading()
+            }
+            is LoadState.Error -> {
+                Error(message = state.error.message ?: "")
+            }
+        }
+        when (val state = photos.loadState.refresh) {
+            is LoadState.NotLoading -> Unit
+            is LoadState.Loading -> {
+                Loading()
+            }
+            is LoadState.Error -> {
+                Error(message = state.error.message ?: "")
+            }
+        }
+        items(photos.itemCount, key = { it }) { index ->
             photos[index]?.let {
                 PhotoItemCard(photo = it, onClickPhoto = onPhotoClick)
             }
         }
+        when (val state = photos.loadState.append) {
+            is LoadState.NotLoading -> Unit
+            is LoadState.Loading -> {
+                Loading()
+            }
+            is LoadState.Error -> {
+                Error(message = state.error.message ?: "")
+            }
+        }
+    }
+}
+
+private fun LazyGridScope.Loading() {
+    item {
+        CircularProgressIndicator(modifier = Modifier.padding(16.dp))
+    }
+}
+
+private fun LazyGridScope.Error(
+    message: String
+) {
+    item {
+        Text(
+            text = message,
+            style = MaterialTheme.typography.h6,
+            color = MaterialTheme.colors.error
+        )
     }
 }
 
@@ -110,8 +160,11 @@ fun PhotoItemCard(
                 model = Builder(LocalContext.current).data(photo.urls.thumb).crossfade(true)
                     .build(),
                 contentDescription = "contentDescription",
-                placeholder = painterResource(drawable.placeholder),
                 modifier = Modifier
+                    .placeholder(
+                        visible = photo.urls.thumb.isEmpty(),
+                        highlight = PlaceholderHighlight.shimmer(),
+                    )
                     .constrainAs(image) {
                         top.linkTo(parent.top)
                         start.linkTo(parent.start)
